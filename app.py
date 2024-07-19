87d20285d204bab80bb45ffb490a82322ee099d0
Tanush M M <tanushmmofficial@gmail.com>
2024-07-19 17:11:18 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, request, jsonify
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/test-get', methods=['GET'])
+@app.route('/test-get', methods=['GET'])
 def test_get():
     data = {'Request Type':'GET',
             'Purpose':f'Tanush API "GET" Request Testing'}
     return jsonify(data)
 
-@application.route('/test-post', methods=['POST'])
+@app.route('/test-post', methods=['POST'])
 def test_post():
     data = request.get_json()
     return jsonify(data)
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
